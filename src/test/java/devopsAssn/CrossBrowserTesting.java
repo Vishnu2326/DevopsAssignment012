e8ed103b9160e98145d843db8077fb8f0424702d
Vishnu2326 <vinu.226@gmail.com>
2024-01-30 15:27:49 +0530

    Devops Asignment

---
+++ src/test/java/devopsAssn/CrossBrowserTesting.java
@@ -0,0 +1,51 @@
+package devopsAssn;
+
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowserTesting {
+
+		 WebDriver driver;
+		  @Parameters({"bname"})
+		  @Test
+		  public void browserTest(String bname) throws MalformedURLException, InterruptedException 
+		  {
+			  if(bname.equalsIgnoreCase("chrome"))
+			  {
+//				  ChromeOptions options=new ChromeOptions();
+//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
+				   driver = new ChromeDriver();
+				  System.out.println("Connection Established with Chrome Browser");
+			  }
+			  if(bname.equalsIgnoreCase("firefox"))
+			  {
+//				  FirefoxOptions options=new FirefoxOptions();
+//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
+				   driver = new FirefoxDriver();
+				  System.out.println("Connection Established with Firefox Browser");
+
+			  }if(bname.equalsIgnoreCase("edge"))
+			  {
+//				  EdgeOptions options=new EdgeOptions();
+//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
+				   driver = new EdgeDriver();
+				  System.out.println("Connection Established with Edge Browser");
+
+			  }
+			  Thread.sleep(5000);
+			  driver.get("https://www.amazon.in");
+			  Thread.sleep(3000);
+			  System.out.println("Application Executing Parallelly!");
+			  driver.quit();
+		  }
+
+	}
+
+
