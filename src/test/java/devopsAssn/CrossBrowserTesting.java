cd5a9ae0bfc4bd0cbaf7a4d5f63708fac45cf198
Vishnu2326 <vinu.226@gmail.com>
2024-01-30 16:54:56 +0530

    Devops Asignment

--- src/test/java/devopsAssn/CrossBrowserTesting.java
+++ src/test/java/devopsAssn/CrossBrowserTesting.java
@@ -5,11 +5,15 @@ import java.net.MalformedURLException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+import org.openqa.selenium.edge.EdgeOptions;
 public class CrossBrowserTesting {
 
 		 WebDriver driver;
@@ -19,23 +23,20 @@ public class CrossBrowserTesting {
 		  {
 			  if(bname.equalsIgnoreCase("chrome"))
 			  {
-//				  ChromeOptions options=new ChromeOptions();
-//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
-				   driver = new ChromeDriver();
+				  ChromeOptions options=new ChromeOptions();
+				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
 				  System.out.println("Connection Established with Chrome Browser");
 			  }
 			  if(bname.equalsIgnoreCase("firefox"))
 			  {
-//				  FirefoxOptions options=new FirefoxOptions();
-//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
-				   driver = new FirefoxDriver();
+				  FirefoxOptions options=new FirefoxOptions();
+				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
 				  System.out.println("Connection Established with Firefox Browser");
 
 			  }if(bname.equalsIgnoreCase("edge"))
 			  {
-//				  EdgeOptions options=new EdgeOptions();
-//				  driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
-				   driver = new EdgeDriver();
+					EdgeOptions options = new EdgeOptions();
+					driver=new RemoteWebDriver(new URL("http://localhost:4444"),options);
 				  System.out.println("Connection Established with Edge Browser");
 
 			  }
